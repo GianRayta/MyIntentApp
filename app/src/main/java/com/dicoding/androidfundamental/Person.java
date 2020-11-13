376b3a7259240a2d9a49536faec54f9789c893d5
GianRayta <gianbinus@yahoo.com>
2020-11-13 13:29:42 +0700

    Update implements parcelable

--- app/src/main/java/com/dicoding/androidfundamental/Person.java
+++ app/src/main/java/com/dicoding/androidfundamental/Person.java
@@ -1,10 +1,14 @@
 package com.dicoding.androidfundamental;
 
-public class Person {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Person implements Parcelable {
     private String name;
     private int age;
     private String email;
     private String city;
+
 
     public Person() {
 
@@ -16,6 +20,38 @@ public class Person {
         }
         this.name = name;
     }*/
+
+    protected Person(Parcel in) {
+        name = in.readString();
+        age = in.readInt();
+        email = in.readString();
+        city = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeInt(age);
+        dest.writeString(email);
+        dest.writeString(city);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Person> CREATOR = new Creator<Person>() {
+        @Override
+        public Person createFromParcel(Parcel in) {
+            return new Person(in);
+        }
+
+        @Override
+        public Person[] newArray(int size) {
+            return new Person[size];
+        }
+    };
 
     public String getName() {
         return name;
